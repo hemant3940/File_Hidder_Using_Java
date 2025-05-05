90e14a3def2ed5acbe70e0479a5b8d6a10c5bfcf
Hemant raj <hemantraj3940@gmail.com>
2025-05-05 20:57:25 +0530

    Add Some Files

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,31 @@
+package db;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection=null;
+    public  static Connection getConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/File_Hider","root","39405417");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection established");
+              return connection;
+    }
+    public static void closeConnection(){
+        if(connection!=null){
+            try{
+                connection.close();
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,22 @@
+package model;
+
+public class User {
+     private String name;
+     private String email;
+     public User(String name,String email){
+         this.name=name;
+         this.email=email;
+     }
+     public void setName(String name){
+         this.name=name;
+     }
+     public String getName(){
+         return name;
+     }
+     public void setEmail(String email){
+         this.email=email;
+     }
+    public String getEmail(){
+        return email;
+    }
+}
